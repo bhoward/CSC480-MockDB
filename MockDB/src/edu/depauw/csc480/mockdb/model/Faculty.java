e56c3310a4496bf753ce9758f3376144b02423e3
Brian Howard <bhoward@depauw.edu>
2022-03-18 10:44:49 -0400

    Working out simulation assumptions

--- MockDB/src/edu/depauw/csc480/mockdb/model/Faculty.java
+++ MockDB/src/edu/depauw/csc480/mockdb/model/Faculty.java
@@ -6,14 +6,12 @@ import java.util.Collection;
 public class Faculty {
   private String name;
   private Department department;
-  private int years;
 
   private Collection<Section> sections;
 
-  public Faculty(String name, Department department, int years) {
+  public Faculty(String name, Department department) {
     this.name = name;
     this.department = department;
-    this.years = years;
 
     this.sections = new ArrayList<>();
 
@@ -26,10 +24,6 @@ public class Faculty {
 
   public Department getDepartment() {
     return department;
-  }
-
-  public int getYears() {
-    return years;
   }
 
   public void addSection(Section section) {
